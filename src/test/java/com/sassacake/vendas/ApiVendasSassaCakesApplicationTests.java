3728e78b333b7409811e2b00f08dbd033d48950e
guilherme_gomes <guilherme_gomes@gitlab.sicredi.net>
2021-02-10 13:35:25 -0300

    Cria projeto Spring

---
+++ src/test/java/com/sassacake/vendas/ApiVendasSassaCakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sassacake.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVendasSassaCakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
